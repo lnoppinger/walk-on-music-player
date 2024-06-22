5b5acc2b8d536b56ebfae9865c7c1b9c2880523a
lnoppinger <lukas.noppinger@gmail.com>
2024-06-22 13:55:51 +0200

    init

--- downloader.py
+++ downloader.py
@@ -1,89 +1,34 @@
-import pygame
-import keyboard
+import json
+from pytube import YouTube
+import subprocess
 import os
 import time
 
-def updateConsole():
-    os.system('cls' if os.name=='nt' else 'clear')
-    print()
-    print("Aktueller Track: ", tracks[currentIndex] if currentIndex >= 0 and currentIndex < len(tracks) and isPlaying else "")
-    print()
-    print("Nächster  Track: ", tracks[nextIndex] if nextIndex >= 0 and nextIndex < len(tracks) else "")
-    print()
-    print()
-    print()
-    if isFadingOut and pygame.mixer.music.get_busy():
-        print("Leertaste:                  Fading Out ...")
-    elif pygame.mixer.music.get_busy():
-        print("Leertaste:                  Fadeout und Pause")
-    else: 
-        print("Leertaste:                  Starten")
-    print("Pfeiltaste rechts/links:    Auswählen des nächsten Tracks")
-    print()
-    print("q:                          Beenden")
+# workaround for age restriction
+from pytube.innertube import _default_clients
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
+errorFile = open("error.txt", "a")
 
-isPlaying = False
-isFadingOut = False
-currentIndex = -1
-nextIndex = 0   
+file = open('data.json')
+data_str = file.read()
+file.close()
+data = json.loads(data_str)
 
-tracks_ = os.listdir("./")
-tracks = []
-for track in tracks_:
-    if track.endswith(".mp3"):
-        tracks.append(track)
+for d in data:
+    try:
+        yt = YouTube(d["video"])
+        yt.streams.filter(only_audio=True).first().download(filename=d["name"] + ".mp4")
+        subprocess.run(
+            'ffmpeg -y -i ' + d["name"] + '.mp4 -ss ' + d["start"] + ' -af "afade=t=in:st=' + d["start"] + ':d=2" ' + d["name"] + '.mp3',
+            stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        
+    except Exception as e:
+        print(d["name"] + " failed to download.")
+        errorFile.write(str(e) + "\n\n")
 
-
-pygame.mixer.init()
-
-updateConsole()
-
-while True:
-    if (not pygame.mixer.music.get_busy()) and (isFadingOut or isPlaying):
-        isFadingOut = False
-        isPlaying = False
-        updateConsole()
-
-    # fuktioniert mit keyboard.read_key() nicht !! (Triggert 2x)
-    keyboardEvent = keyboard.read_event()
-    key = keyboardEvent.name
-
-    if not keyboardEvent.event_type == "down":
-        pass
-
-    elif(key == "space" and not pygame.mixer.music.get_busy()):
-        isFadingOut = False
-        if nextIndex >= len(tracks):
-            break
-        pygame.mixer.music.load(os.path.abspath("./" + tracks[nextIndex]))
-        pygame.mixer.music.play()
-        isPlaying = True
-        currentIndex = nextIndex
-        nextIndex += 1
-        if nextIndex >= len(tracks):
-            nextIndex = 0
-
-    elif(key == "space" and not isFadingOut):
-        isFadingOut = True
-        updateConsole()
-        pygame.mixer.music.fadeout(3*1000)
-        time.sleep(3-0.1)
-        pygame.mixer.music.stop()
-        isPlaying = False
-        isFadingOut = False
-
-    elif(key == "q" or key == "Q"):   
-        break
-
-    elif(key == "nach-rechts"):
-        nextIndex += 1
-        if nextIndex >= len(tracks):
-            nextIndex = 0
-    
-    elif(key == "nach-links"):
-        nextIndex -= 1
-        if nextIndex < 0:
-            nextIndex = len(track)
-    
-    updateConsole()
+errorFile.close()
+time.sleep(10)
+for file in os.listdir("./"):
+    if file.endswith(".mp4"):
+        os.remove(file)
